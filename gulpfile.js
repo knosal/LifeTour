2128734091d3f60001bc3d9cf175d05e422bc9fb
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-08-19 22:32:07 +0300

    Подготовк работы проекта

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 import gulp from 'gulp';
 import browserSync from 'browser-sync';
 import del from 'del';
-import {compileStyles, compileMinStyles} from './gulp/compileStyles.mjs';
+import { compileStyles, compileMinStyles } from './gulp/compileStyles.mjs';
 import { copy, copyImages, copySvg } from './gulp/copyAssets.mjs';
-import {compileMainMinScripts, compileMainScripts, compileVendorScripts} from './gulp/compileScripts.mjs';
-import {optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg} from './gulp/optimizeImages.mjs';
+import { compileMainMinScripts, compileMainScripts, compileVendorScripts } from './gulp/compileScripts.mjs';
+import { optimizeSvg, sprite, createWebp, optimizePng, optimizeJpg } from './gulp/optimizeImages.mjs';
 
 const server = browserSync.create();
 const streamStyles = () => compileStyles().pipe(server.stream());
@@ -14,7 +14,7 @@ const clean = () => del('build');
 const syncServer = () => {
   server.init({
     server: 'build/',
-    index: 'sitemap.html',
+    index: 'index.html',
     notify: false,
     open: true,
     cors: true,
@@ -43,4 +43,4 @@ const build = gulp.series(clean, copy, sprite, gulp.parallel(compileMinStyles, c
 const dev = gulp.series(clean, copy, sprite, gulp.parallel(compileMinStyles, compileMainMinScripts, compileVendorScripts, optimizePng, optimizeJpg, optimizeSvg), syncServer);
 const start = gulp.series(clean, copy, sprite, gulp.parallel(compileStyles, compileMainScripts, compileVendorScripts), syncServer);
 
-export { createWebp as webp, build, start, dev};
+export { createWebp as webp, build, start, dev };
--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,4 +1,3 @@
-// Swiper 7.4.1
-import './vendor/swiper';
-import './vendor/focus-visible-polyfill';
+import './vendor/swiper'; // Swiper 7.4.1
+import './vendor/leaflet'; // Leaflet 1.9.4
 

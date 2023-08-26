7c281630ca3ffd983c4ddbdd8c6ac3bf3aedbcba
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-08-26 15:17:09 +0300

    Реализация блока header

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,7 @@
 import { iosVhFix } from './utils/ios-vh-fix';
 import { Form } from './modules/form-validate/form';
+import { createScript } from './modules/dev';
+import { initButtonMenu } from './modules/menuToggle';
 
 // ---------------------------------
 
@@ -16,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
+    // createScript();
+    initButtonMenu();
     const form = new Form();
     window.form = form;
     form.init();
---
+++ source/js/modules/menuToggle.js
@@ -0,0 +1,35 @@
+const initButtonMenu = () => {
+  const isOpened = 'is-opened';
+  const header = document.querySelector('.header');
+  const container = header.querySelector('.header__container');
+  const toggleBtn = header.querySelector('.toggle-button');
+  const nav = header.querySelector('.navigation');
+  const navLink = header.querySelector('.navigation a');
+
+  const isEscapeKey = (evt) => evt.key === 'Escape';
+  const onModalEscKeydown = (evt) => isEscapeKey(evt) ? closeMenu() : null;
+  const onLinkClick = (evt) => evt.target.matches(navLink) ? closeMenu() : null;
+  const clickActionToggle = () => toggleBtn.classList.contains(isOpened) ? closeMenu() : openMenu();
+
+  const openMenu = () => {
+    container.classList.add(isOpened);
+    toggleBtn.classList.add(isOpened);
+    nav.classList.add(isOpened);
+
+    document.addEventListener('keydown', onModalEscKeydown);
+    nav.addEventListener('click', onLinkClick);
+  };
+
+  const closeMenu = () => {
+    container.classList.remove(isOpened);
+    toggleBtn.classList.remove(isOpened);
+    nav.classList.remove(isOpened);
+
+    document.removeEventListener('keydown', onModalEscKeydown);
+    nav.removeEventListener('click', onLinkClick);
+  };
+
+  toggleBtn.addEventListener('click', clickActionToggle);
+};
+
+export { initButtonMenu };

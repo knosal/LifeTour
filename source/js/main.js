3071e772613f7a7f2d3faf11f2a6d2f1a1f2891b
Knosal.V <35274816+knosal@users.noreply.github.com>
2023-09-04 11:13:40 +0300

    Отключил проверку pp

--- source/js/main.js
+++ source/js/main.js
@@ -33,7 +33,7 @@ window.addEventListener('DOMContentLoaded', () => {
     window.form = form;
     form.init();
 
-    createScript();
+    // createScript();
     initButtonMenu();
     initVideoPlayer();
     initAudioPlayer();
